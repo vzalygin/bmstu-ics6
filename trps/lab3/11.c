7ab491c58fe1eb4ec601e5a2268334ae19ab354f
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-05-03 08:25:16 +0000

    trps lab3

---
+++ trps/lab3/11.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <math.h>
+
+int main()
+{
+    for (int __ = 0; __ < 100000; __++) {
+        double s,a,m,eps,x;
+        double *ms, *mss;
+        int N = 1000, i,o; 
+        ms = (double*)malloc(N * sizeof(int));
+        mss = (double*)malloc(N * sizeof(int));
+        eps = 0.00001;
+        ms[1] = 1.0/4;
+        o = 1;
+        s = 1.0/4;
+        mss[1] = s;
+        do {
+            o++;
+            m = -1/(4*o);
+            ms[o] = ms[o-1]*m;
+            s = s+ms[o];
+            mss[o] = s;
+        } while (abs(ms[o-1]-ms[o])>=eps);
+        printf("s = %f\nn = %d\n",s,o);
+    }
+    return 0;
+}
