45ca9dd10ca1e4b8faaf946be5f9911a96b283e2
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-11-09 00:43:09 +0300

    shemach lab3 and network lab 3

---
+++ schemach/2sem/lab3/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "/mnt/c/repos/bmstu-ics6/schemach/2sem/lab3/main_tb_isim_beh.exe" -prj "/mnt/c/repos/bmstu-ics6/schemach/2sem/lab3/main_tb_beh.prj" "work.main_tb" "work.glbl" 
